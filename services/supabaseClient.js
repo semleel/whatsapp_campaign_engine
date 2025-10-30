c7504d35f7c2238c109509c35e1d633eabecbc51
Karkar-nub <zhenghong1202003@gmail.com>
2025-10-30 23:46:44 +0800

    yisempogai

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from "dotenv";
+dotenv.config();
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
